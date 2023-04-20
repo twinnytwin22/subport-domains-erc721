e19fd8c54a3f2dcc90dc5e9f81c1cdee640e2239
Randal Herndon / Twinny Twin <97874795+twinnytwin22@users.noreply.github.com>
2023-04-20 01:35:45 -0700

    init

---
+++ .history/hardhat.config_20230420013030.js
@@ -0,0 +1,18 @@
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-ethers');
+require('dotenv').config();
+
+module.exports = {
+  solidity: '0.8.10',
+  networks: {
+    mumbai: {
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      chainId: 1,
+      url: process.env.PROD_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
